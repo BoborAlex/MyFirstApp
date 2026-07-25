89508a1db9aee7156d86214c5ee35ecafa3b78ec
BoborAlex <boboralex@mail.ru>
2026-07-25 11:27:23 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
 }
 
 android {
@@ -33,7 +34,15 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    // Добавляем эту строчку:
     implementation("com.google.android.gms:play-services-location:21.1.0")
     implementation("com.squareup.picasso:picasso:2.8")
+
+    // Room (Локальная база данных)
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+
+    // WorkManager (Фоновые задачи)
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
 }
